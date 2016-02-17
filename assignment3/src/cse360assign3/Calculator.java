5aca2aa39c5711426c83e4450a7f265ad4addb1d
Tom Gottschlich <TomGottschlich@gmail.com>
2016-02-16 20:07:37 -0700

    Step 2

--- assignment3/src/cse360assign3/Calculator.java
+++ assignment3/src/cse360assign3/Calculator.java
@@ -1,35 +1,83 @@
-
 package cse360assign3;
+/**
+ * Class to calculate various mathematical operations
+ * 
+ * @author Thomas Gottschlich for CSE360 
+ * @version Feb 16 2016 
+ * @pin 326
+ */
 
-public class Calculator {
+public class Calculator 
+{
 
 	private int total;
 	
-	public Calculator () {
+	/**
+	 *  Creates a calculator with an initial total of zero.
+	 */
+	public Calculator () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
+	/**
+	 * getTotal - gets total from calculator
+	 * 
+	 * @return 0
+	 */
+	public int getTotal () 
+	{
 		return 0;
 	}
 	
-	public void add (int value) {
+	/**
+	 * add - adds number to total
+	 * 
+	 * @param value - int to add
+	 */
+	public void add (int value) 
+	{
 		
 	}
 	
-	public void subtract (int value) {
+	/**
+	 * subtract - subtracts number from total
+	 * 
+	 * @param value - int to subtract
+	 */
+	public void subtract (int value) 
+	{
 		
 	}
 	
-	public void multiply (int value) {
+	/**
+	 * multiply - multiplies the total by a number
+	 * 
+	 * @param value - int to multiply by
+	 */
+	public void multiply (int value) 
+	{
 		
 	}
 	
-	public void divide (int value) {
+	/**
+	 * divide - divides the total by a number
+	 * 
+	 * @param value - int to divide by
+	 */
+	public void divide (int value) 
+	{
 		
 	}
 	
-	public String getHistory () {
+	/**
+	 * getHistory - returns a string (currently an empty string)
+	 * 
+	 * @return ""
+	 */
+	public String getHistory () 
+	{
 		return "";
 	}
+	
 }
---
+++ assignment3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,54 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest 
+{
+
+	@Test
+	public void testCalculator() 
+	{
+		Calculator calc =  new Calculator(); 
+		
+		assertNotNull(calc);
+	}
+	
+	@Test
+	public void getTotal() 
+	{
+		fail("Not implimented");
+	}
+
+	@Test
+	public void add() 
+	{
+		fail("Not implimented");
+	}
+	
+	@Test
+	public void subtract() 
+	{
+		fail("Not implimented");
+	}
+	
+	@Test
+	public void multiply() 
+	{
+		fail("Not implimented");
+	}
+	
+	@Test
+	public void divide() 
+	{
+		fail("Not implimented");
+	}
+	
+	@Test
+	public void getHistory() 
+	{
+		fail("Not implimented");
+	}
+	
+}
